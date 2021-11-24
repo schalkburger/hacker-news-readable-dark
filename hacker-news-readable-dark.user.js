96b4cc78ca5dacfa2281bd6cf329000b1b3ec407
Schalk Burger <schalkb@gmail.com>
2021-11-24 17:16:30 +0200

    Update comment shadow

--- hacker-news-readable-dark.user.js
+++ hacker-news-readable-dark.user.js
@@ -30,6 +30,7 @@
         --color-charcoal: #333b4e;
         --color-darkgunmental: #191e2a;
         --color-davysgrey: #4f535e;
+        --shadow-color: 223deg 30% 8%;
     }
 
     body,
@@ -102,7 +103,7 @@
     .subtext a,
     .subtext .score,
     .subtext .age a {
-        color: #7a7b7e !important;
+        color: #8d9099 !important;
     }
 
     .subtext {
@@ -160,10 +161,10 @@
     .comment-tree .comment {
         background-color: #1f2430;
         padding: 40px;
-        box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.02),
-        0 6.7px 5.3px rgba(0, 0, 0, 0.028), 0 12.5px 10px rgba(0, 0, 0, 0.035),
-        0 22.3px 17.9px rgba(0, 0, 0, 0.042), 0 41.8px 33.4px rgba(0, 0, 0, 0.05),
-        0 100px 80px rgba(0, 0, 0, 0.07);
+        box-shadow: 0.3px 0.5px 0.7px hsl(var(--shadow-color) / 0.32),
+        0.8px 1.6px 2px -0.8px hsl(var(--shadow-color) / 0.32),
+        2px 3.9px 4.9px -1.7px hsl(var(--shadow-color) / 0.32),
+        4.8px 9.5px 12px -2.5px hsl(var(--shadow-color) / 0.32);
     }
 
     tr.athing:nth-child(even) td .comment {
