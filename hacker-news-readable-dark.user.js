00ac7af26ae7748108674a10e7e1ed7451c88c7b
Schalk Burger <schalkb@gmail.com>
2022-12-26 01:46:24 +0200

    Refactoring

--- hacker-news-readable-dark.user.js
+++ hacker-news-readable-dark.user.js
@@ -3,35 +3,31 @@
 // @namespace    https://greasyfork.org/en/users/961305-darkharden
 // @include      https://news.ycombinator.com/*
 // @match        https://news.ycombinator.com/*
-// @version      1.0.2
+// @version      2.0.1
 // @author       Schalk Burger <schalkb@gmail.com>
 // @description  Hacker News Readable Dark
 // @license MIT
 // ==/UserScript==
 
+// To-do
+// Add click event listener that opens link and comments
+
 (function () {
     "use strict";
-    console.log("Hacker News Readable Dark");
-    let cells = document.getElementsByTagName("td");
-    for (let i = 0; i < cells.length; i++) {
-        let td = cells[i];
-        if ('title' === td.className) {
-            let links = td.getElementsByTagName('a');
-            console.log("links -->", links);
-            if (links.length > 0) {
-                let link = links[0];
-                link.setAttribute("target", "_blank");
-                console.log("link -->", link);
-            }
-        }
-        if ('subtext' === td.className) {
-            let commentsLinks = td.getElementsByTagName('a');
-            console.log("commentsLinks -->", commentsLinks);
-            if (commentsLinks.length > 0) {
-                let commentsLink = commentsLinks[1];
-                commentsLink.setAttribute("target", "_blank");
-                console.log("commentsLink -->", commentsLink);
-            }
-        }
+    console.log("Hacker News Readable Dark 2.0.1");
+    // Open Hacker News links in new tab
+    const titleLink = document.querySelectorAll(".titleline a");
+    for (let i = 0; i < titleLink.length; i++) {
+        const theLink = titleLink[i];
+        theLink.setAttribute("target", "_blank");
+        console.log("theLink:", theLink);
     }
+    // Open Hacker News comments in new tab
+    const commentLink = document.querySelectorAll(".age a");
+    for (let i = 0; i < commentLink.length; i++) {
+        const theCommentLink = commentLink[i];
+        theCommentLink.setAttribute("target", "_blank");
+        console.log("theCommentLink:", theCommentLink);
+    }
+
 })();
