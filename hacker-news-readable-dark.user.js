288c980f19eabeb8ad03ddab2e291e8428581ac8
Schalk Burger <schalkb@gmail.com>
2022-12-27 01:58:41 +0200

    Refactor open new tab logic

--- hacker-news-readable-dark.user.js
+++ hacker-news-readable-dark.user.js
@@ -3,31 +3,36 @@
 // @namespace    https://greasyfork.org/en/users/961305-darkharden
 // @include      https://news.ycombinator.com/*
 // @match        https://news.ycombinator.com/*
-// @version      2.0.1
+// @version      2.0.9
 // @author       Schalk Burger <schalkb@gmail.com>
 // @description  Hacker News Readable Dark
 // @license MIT
 // ==/UserScript==
 
 // To-do
-// Add click event listener that opens link and comments
+// Add click event listener that opens links and comments
 
 (function () {
     "use strict";
-    console.log("Hacker News Readable Dark 2.0.1");
+    // Init script
+    let version = GM_info.script.version;
+    console.log(`Hacker News Readable Dark ${version}`);
     // Open Hacker News links in new tab
-    const titleLink = document.querySelectorAll(".titleline a");
-    for (let i = 0; i < titleLink.length; i++) {
-        const theLink = titleLink[i];
-        theLink.setAttribute("target", "_blank");
-        console.log("theLink:", theLink);
+    const titleLinks = document.querySelectorAll(".athing a, .subtext a");
+    for (let i = 0; i < titleLinks.length; i++) {
+        const titleLink = titleLinks[i];
+        titleLink.setAttribute("target", "_blank");
+        // console.log("titleLink:", titleLink);
+        // Open link + comments with one click
+        titleLink.addEventListener("click", (e) => {
+            e.preventDefault();
+            console.log("Hacker News link clicked");
+            function openLinkAndComments() {
+                console.log("openLinkAndComments function ran")
+            }
+            setTimeout(openLinkAndComments, 100);
+        });
     }
-    // Open Hacker News comments in new tab
-    const commentLink = document.querySelectorAll(".age a");
-    for (let i = 0; i < commentLink.length; i++) {
-        const theCommentLink = commentLink[i];
-        theCommentLink.setAttribute("target", "_blank");
-        console.log("theCommentLink:", theCommentLink);
-    }
+
 
 })();
