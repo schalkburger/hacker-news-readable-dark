85642a0854d72230dca09cbf80a971bede8fc142
Schalk Burger <schalkb@gmail.com>
2022-12-27 18:23:02 +0200

    Push links array test

--- hacker-news-readable-dark.user.js
+++ hacker-news-readable-dark.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://greasyfork.org/en/users/961305-darkharden
 // @include      https://news.ycombinator.com/*
 // @match        https://news.ycombinator.com/*
-// @version      2.0.11
+// @version      2.0.13
 // @author       Schalk Burger <schalkb@gmail.com>
 // @description  Hacker News Readable Dark
 // @license MIT
@@ -18,52 +18,64 @@
     // Init script
     let version = GM_info.script.version;
     console.log(`Hacker News Readable Dark ${version}`);
-    // Add target blank to links
-    const titleLinks = document.querySelectorAll(".titleline a");
+    // titleLinks
+    const titleLinks = document.querySelectorAll(".titleline > a");
     for (let i = 0; i < titleLinks.length; i++) {
         const titleLink = titleLinks[i];
         titleLink.setAttribute("target", "_blank");
-        const titleLinkHref = titleLink.href;
-        // console.log("titleLinkHref", titleLinkHref)
+        titleLink.classList.add("news-link");
+
     }
-    // Add target blank to comments
-    const commentLinks = document.querySelectorAll(".age a");
-    for (let i = 0; i < commentLinks.length; i++) {
-        const commentLink = commentLinks[i];
-        commentLink.setAttribute("target", "_blank");
-        const commentLinkHref = commentLink.href;
-        // console.log("commentLinkHref", commentLinkHref)
-    }
-    // Add target blank to comments
-    const subTextElements = document.querySelectorAll(".subtext");
-    for (let i = 0; i < subTextElements.length; i++) {
-        const subTextElement = subTextElements[i];
-        subTextElement.classList.add("subtext-element")
-        // console.log("subTextElementHref", subTextElementHref)
+
+    const newsLinks = document.querySelectorAll(".news-link");
+    for (let i = 0; i < newsLinks.length; i++) {
+        const newsLink = newsLinks[i];
+        const newsLinkHref = newsLink.href;
+        console.log("newsLinkHref", newsLinkHref)
+        // singleClickLink.setAttribute("href", newsLinkHref);
     }
 
     // Insert L+C link
-    const linkCommentSpan = document.createElement("span");
-    linkCommentSpan.innerHTML = `<a href="#" class="link-comment">[L+C]</a>`;
-    const linkCommentContainer = document.querySelector(".subline");
-    const linkCommentParent = linkCommentContainer.parentNode;
-    linkCommentParent.insertBefore(linkCommentSpan, linkCommentContainer.nextSibling);
-    //
-    const newsLinks = document.querySelectorAll(".titleline > a");
-    for (let i = 0; i < newsLinks.length; i++) {
-        const newsLink = newsLinks[i];
-        newsLink.setAttribute("target", "_blank");
-        const newsLinkHref = newsLink.href;
-        console.log("newsLinkHref", newsLinkHref);
-        const linkCommentSpanAnchor = document.querySelector(".link-comment");
-        function setLinkCommentSpanAnchor() {
-            linkCommentSpanAnchor.setAttribute("href", newsLinkHref);
-            linkCommentSpan.addEventListener("click", function () {
-                console.log("L+C link clicked");
-                // window.open("https://cnn.com");
-            });
+    const subTextLinks = document.querySelectorAll(".subline");
+    for (let i = 0; i < subTextLinks.length; i++) {
+        const subTextLink = subTextLinks[i];
+        //
+        const linkCommentSpan = document.createElement("span");
+        linkCommentSpan.innerHTML = `<a href="#" target="_blank" class="link-comment">[L+C]</a>`;
+        const linkCommentParent = subTextLink.parentNode;
+        linkCommentParent.append(linkCommentSpan);
+        const subTextCommentLinks = document.querySelectorAll(".age a");
+        const linkComments = document.querySelectorAll(".link-comment");
+        let arrHREF = []; // create an Array to save hrefs
+        let j = 0;
+        for (; j < subTextCommentLinks.length; j++) {
+            arrHREF.push(subTextCommentLinks[j].href); // push hrefs in array
+            console.log("subTextCommentLink", subTextCommentLinks[j].href)
+            let k = 0;
+            for (; k < linkComments.length; k++) {
+                const linkComment = linkComments[k];
+                arrHREF.push(linkComments[k].href); // push hrefs in array
+                linkComment.setAttribute("href", subTextCommentLinks[j].href);
+                console.log("linkComments", subTextCommentLinks[j].href)
+            }
         }
-        setTimeout(setLinkCommentSpanAnchor, 500);
     }
+
+    // singleClickLink
+    // const singleClickLinks = document.querySelectorAll(".link-comment");
+    // for (let i = 0; i < singleClickLinks.length; i++) {
+    //     const singleClickLink = singleClickLinks[i];
+    //     const newsLinks = document.querySelectorAll(".news-link");
+    //     for (let i = 0; i < newsLinks.length; i++) {
+    //         const newsLink = newsLinks[i];
+    //         const newsLinkHref = newsLink.href;
+    //         singleClickLink.setAttribute("href", newsLinkHref);
+    //     }
+    //     singleClickLink.addEventListener('mousedown', function (e) {
+    //         e.preventDefault();
+    //         console.log("singleClickLink clicked")
+    //     });
+    // }
+
 
 })();
