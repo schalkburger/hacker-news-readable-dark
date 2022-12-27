4a09054e03a0a9f6a4d24cf532a92febfa163134
Schalk Burger <schalkb@gmail.com>
2022-12-27 17:06:52 +0200

    L+C

--- hacker-news-readable-dark.user.js
+++ hacker-news-readable-dark.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://greasyfork.org/en/users/961305-darkharden
 // @include      https://news.ycombinator.com/*
 // @match        https://news.ycombinator.com/*
-// @version      2.0.10
+// @version      2.0.11
 // @author       Schalk Burger <schalkb@gmail.com>
 // @description  Hacker News Readable Dark
 // @license MIT
@@ -18,24 +18,52 @@
     // Init script
     let version = GM_info.script.version;
     console.log(`Hacker News Readable Dark ${version}`);
-    // Open Hacker News links in new tab
-    const titleLinks = document.querySelectorAll(".athing a, .subtext a");
+    // Add target blank to links
+    const titleLinks = document.querySelectorAll(".titleline a");
     for (let i = 0; i < titleLinks.length; i++) {
         const titleLink = titleLinks[i];
         titleLink.setAttribute("target", "_blank");
-        // console.log("titleLink:", titleLink);
-        const commentLinks = document.querySelectorAll(".age a");
-        for (let i = 0; i < commentLinks.length; i++) {
-            const commentLink = commentLinks[i];
-            console.log("commentLink", commentLink.href)
-        }
-
-        // Open link + comments with one click
-        titleLink.addEventListener("click", function () {
-            console.log("Hacker News link clicked");
-            // window.open("https://cnn.com");
-        });
+        const titleLinkHref = titleLink.href;
+        // console.log("titleLinkHref", titleLinkHref)
+    }
+    // Add target blank to comments
+    const commentLinks = document.querySelectorAll(".age a");
+    for (let i = 0; i < commentLinks.length; i++) {
+        const commentLink = commentLinks[i];
+        commentLink.setAttribute("target", "_blank");
+        const commentLinkHref = commentLink.href;
+        // console.log("commentLinkHref", commentLinkHref)
+    }
+    // Add target blank to comments
+    const subTextElements = document.querySelectorAll(".subtext");
+    for (let i = 0; i < subTextElements.length; i++) {
+        const subTextElement = subTextElements[i];
+        subTextElement.classList.add("subtext-element")
+        // console.log("subTextElementHref", subTextElementHref)
     }
 
+    // Insert L+C link
+    const linkCommentSpan = document.createElement("span");
+    linkCommentSpan.innerHTML = `<a href="#" class="link-comment">[L+C]</a>`;
+    const linkCommentContainer = document.querySelector(".subline");
+    const linkCommentParent = linkCommentContainer.parentNode;
+    linkCommentParent.insertBefore(linkCommentSpan, linkCommentContainer.nextSibling);
+    //
+    const newsLinks = document.querySelectorAll(".titleline > a");
+    for (let i = 0; i < newsLinks.length; i++) {
+        const newsLink = newsLinks[i];
+        newsLink.setAttribute("target", "_blank");
+        const newsLinkHref = newsLink.href;
+        console.log("newsLinkHref", newsLinkHref);
+        const linkCommentSpanAnchor = document.querySelector(".link-comment");
+        function setLinkCommentSpanAnchor() {
+            linkCommentSpanAnchor.setAttribute("href", newsLinkHref);
+            linkCommentSpan.addEventListener("click", function () {
+                console.log("L+C link clicked");
+                // window.open("https://cnn.com");
+            });
+        }
+        setTimeout(setLinkCommentSpanAnchor, 500);
+    }
 
 })();
