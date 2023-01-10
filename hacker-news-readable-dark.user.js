bc716b880278ab5cca1cec2527afff2f80ace971
Schalk Burger <schalkb@gmail.com>
2023-01-10 21:43:11 +0200

    Add target blank to comment links

--- hacker-news-readable-dark.user.js
+++ hacker-news-readable-dark.user.js
@@ -10,69 +10,71 @@
 // ==/UserScript==
 
 (function () {
-    "use strict";
-    // Init script
-    let version = GM_info.script.version;
-    console.log(`Hacker News Readable Dark ${version}`);
-    // titleLinks
-    const aThings = document.querySelectorAll(".athing");
-    for (let i = 0; i < aThings.length; i++) {
-        const aThing = aThings[i];
-        aThing.classList.add("news-link");
-        // Open new link and add class
-        const titleLineLinks = document.querySelectorAll(".titleline > a");
-        for (let i = 0; i < titleLineLinks.length; i++) {
-            const titleLineLink = titleLineLinks[i];
-            titleLineLink.setAttribute("target", "_blank");
-        }
-        // Get link
-        // Get comments ID
-        const aThingID = aThing.id;
-        const linkCommentSpan = document.createElement("td");
-        linkCommentSpan.setAttribute("id", aThingID);
-        linkCommentSpan.className = "link-comment-td";
-        linkCommentSpan.innerHTML = `<a href="${aThingID}" comments-id="${aThingID}" target="_blank" title="Open link + comments" class="link-comment">[l+c]</a>`;
-        aThing.append(linkCommentSpan);
-        // linkCommentSpanAnchors
-        const linkCommentSpanAnchors = document.querySelectorAll(".link-comment");
-        for (let i = 0; i < linkCommentSpanAnchors.length; i++) {
-            const linkCommentSpanAnchor = linkCommentSpanAnchors[i];
-            linkCommentSpanAnchor.setAttribute("style", "padding: 10px;")
-        }
+  "use strict";
+  // Init script
+  let version = GM_info.script.version;
+  console.log(`Hacker News Readable Dark ${version}`);
+  // titleLinks
+  const aThings = document.querySelectorAll(".athing");
+  for (let i = 0; i < aThings.length; i++) {
+    const aThing = aThings[i];
+    aThing.classList.add("news-link");
+    // Open new link and add class
+    const titleLineLinks = document.querySelectorAll(".titleline > a");
+    for (let i = 0; i < titleLineLinks.length; i++) {
+      const titleLineLink = titleLineLinks[i];
+      titleLineLink.setAttribute("target", "_blank");
     }
-    // Get story link
-    const titleLineHrefs = document.querySelectorAll(".titleline > a");
-    var hrefsArray = []; // create an Array to save hrefs
-    var j = 0;
-    for (; j < titleLineHrefs.length; j++) {
-        hrefsArray.push(titleLineHrefs[j].href); // push hrefs in array
+    const subLineLinks = document.querySelectorAll(".subtext a");
+    for (let i = 0; i < subLineLinks.length; i++) {
+      const subLineLink = subLineLinks[i];
+      subLineLink.setAttribute("target", "_blank");
     }
-    const newsLinks = document.querySelectorAll(".link-comment-td a");
-    for (let i = 0; i < newsLinks.length; i++) {
-        const newsLink = newsLinks[i];
-        newsLink.setAttribute("href", hrefsArray[i])
+    // Get link
+    // Get comments ID
+    const aThingID = aThing.id;
+    const linkCommentSpan = document.createElement("td");
+    linkCommentSpan.setAttribute("id", aThingID);
+    linkCommentSpan.className = "link-comment-td";
+    linkCommentSpan.innerHTML = `<a href="${aThingID}" comments-id="${aThingID}" target="_blank" title="Open link + comments" class="link-comment">[l+c]</a>`;
+    aThing.append(linkCommentSpan);
+    // linkCommentSpanAnchors
+    const linkCommentSpanAnchors = document.querySelectorAll(".link-comment");
+    for (let i = 0; i < linkCommentSpanAnchors.length; i++) {
+      const linkCommentSpanAnchor = linkCommentSpanAnchors[i];
+      linkCommentSpanAnchor.setAttribute("style", "padding: 10px;");
     }
-    // Open comments + link
-    const singleClickLinks = document.querySelectorAll(".link-comment-td a");
-    for (let i = 0; i < singleClickLinks.length; i++) {
-        const singleClickLink = singleClickLinks[i];
-        const singleClickLinkCommentsID = singleClickLink.getAttribute("comments-id");
-        const singleClickLinkCommentsIDLink = `https://news.ycombinator.com/item?id=${singleClickLinkCommentsID}`
-        const singleClickLinkNewsLink = singleClickLink.getAttribute("href");
-        singleClickLink.addEventListener('click', function (e) {
-            e.preventDefault();
-            // console.log("singleClickLink clicked")
-            // console.log("singleClickLinkCommentsIDLink", singleClickLinkCommentsIDLink)
-            // console.log("singleClickLinkNewsLink", singleClickLinkNewsLink)
-            window.open(singleClickLinkCommentsIDLink)
-            window.open(singleClickLinkNewsLink)
-        });
-    }
-    // Remove [l+c] links from comments page
-    // comment-tree
-    const commentTreeLinks = document.querySelectorAll(".comment-tree .link-comment, .fatitem .link-comment");
-    for (let i = 0; i < commentTreeLinks.length; i++) {
-        const commentTreeLink = commentTreeLinks[i];
-        commentTreeLink.remove();
-    }
+  }
+  // Get story link
+  const titleLineHrefs = document.querySelectorAll(".titleline > a");
+  var hrefsArray = []; // create an Array to save hrefs
+  var j = 0;
+  for (; j < titleLineHrefs.length; j++) {
+    hrefsArray.push(titleLineHrefs[j].href); // push hrefs in array
+  }
+  const newsLinks = document.querySelectorAll(".link-comment-td a");
+  for (let i = 0; i < newsLinks.length; i++) {
+    const newsLink = newsLinks[i];
+    newsLink.setAttribute("href", hrefsArray[i]);
+  }
+  // Open comments + link
+  const singleClickLinks = document.querySelectorAll(".link-comment-td a");
+  for (let i = 0; i < singleClickLinks.length; i++) {
+    const singleClickLink = singleClickLinks[i];
+    const singleClickLinkCommentsID = singleClickLink.getAttribute("comments-id");
+    const singleClickLinkCommentsIDLink = `https://news.ycombinator.com/item?id=${singleClickLinkCommentsID}`;
+    const singleClickLinkNewsLink = singleClickLink.getAttribute("href");
+    singleClickLink.addEventListener("click", function (e) {
+      e.preventDefault();
+      window.open(singleClickLinkCommentsIDLink);
+      window.open(singleClickLinkNewsLink);
+    });
+  }
+  // Remove [l+c] links from comments page
+  // comment-tree
+  const commentTreeLinks = document.querySelectorAll(".comment-tree .link-comment, .fatitem .link-comment");
+  for (let i = 0; i < commentTreeLinks.length; i++) {
+    const commentTreeLink = commentTreeLinks[i];
+    commentTreeLink.remove();
+  }
 })();
